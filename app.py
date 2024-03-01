d5b9239efdd87b5fa8444c1a2e2477bd585de289
Rachel Weissmandl <contactmeatrachelw@gmail.com>
2024-03-01 17:20:33 +0000

    return specified data points

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import requests
 
-postcode = "M74FR"
-url = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/cb2"
-
+postcode = "EC4M7RF"
+url = f"https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/{postcode}"
 headers = {
     "Host": "uk.api.just-eat.io",
     "User-Agent": "Foo bar"
 }
 
 response = requests.get(url, headers=headers)
+print(response)
 
-print (response.json())
+for restaurant in (response.json()["restaurants"])[0:10]:
+    print (restaurant["name"])
+    print (restaurant["address"]["firstLine"])
+    print(restaurant["address"]["postalCode"])
+    print(restaurant["rating"]["starRating"])
+    print(restaurant["cuisines"][0]["name"])
