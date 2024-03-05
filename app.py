e95ea7a1581e6854fcf7f02608730398bb956818
Rachel Weissmandl <contactmeatrachelw@gmail.com>
2024-03-05 19:50:02 +0000

    form to search from any postcode

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from get_request_to_api import *
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route ('/nearbyrestaurants')
+@app.route ('/', methods=['GET', 'POST'])
 def find_restaurants():
-    find_restaurants = find_local_restaurants("EC4M7RF")
+    postcode = request.form.get('postcode', 'EC4M7RF')
+    find_restaurants = find_local_restaurants(postcode)
     find_restaurants.get_request()
     closest_restaurants = find_restaurants.closest_restaurants(10)
-    return render_template("index.html", form=closest_restaurants)
+    return render_template("index.html", form=closest_restaurants, postcode=postcode)
 
 if __name__ == "__main__":
     app.run()
